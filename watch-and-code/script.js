63a328cdad2dadfe29961f0f83fb5ac70d1e01fd
John Osborne <johnosborne1456@gmail.com>
2018-07-01 14:25:15 -0400

    version 11

--- watch-and-code/script.js
+++ watch-and-code/script.js
@@ -20,25 +20,35 @@ var todoList = {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
     
-    // Get number of completed todos.
-    for (var i = 0; i < totalTodos; i++) {
-      if (this.todos[i].completed === true) {
+    this.todos.forEach(function(todo) {
+      if (todo.completed === true) {
         completedTodos++;
       }
-    }
+    });
     
     // Case 1: If everything’s true, make everything false.
-    if (completedTodos === totalTodos) {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = false;
+    /*if (completedTodos === totalTodos) {
+      this.todos.forEach(function(todo) {
+        todo.completed = false;
       }
-    // Case 2: Otherwise, make everything true.
+;;   // Case 2: Otherwise, make everything true.
     } else {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = true;
-      }      
-    }
-  }
+
+     
+this.todos.forEach(function(todo) {
+        todo.completed = true;
+      });    }
+*/
+    
+    this.todos.forEach(function(todo) {
+      // Case 1: If everything’s true, make everything false.
+      if (completedTodos === totalTodos) {
+        todo.completed = false;
+      // Case 2: Otherwise, make everything true.
+      } else {
+        todo.completed = true;
+      }
+    });  }
 };
 
 var handlers = {
@@ -77,7 +87,7 @@ var view = {
   displayTodos: function() {
     var todosUl = document.querySelector('ul');
     todosUl.innerHTML = '';
-    for (var i = 0; i < todoList.todos.length; i++) {
+    /*for (var i = 0; i < todoList.todos.length; i++) {
       var todoLi = document.createElement('li');
       var todo = todoList.todos[i];
       var todoTextWithCompletion = '';
@@ -92,7 +102,22 @@ var view = {
       todoLi.textContent = todoTextWithCompletion;
       todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
-    }   
+    }*/
+    todoList.todos.forEach(function(todo, position) {
+      var todoLi = document.createElement('li');
+      var todoTextWithCompletion = '';
+
+      if (todo.completed === true) {
+        todoTextWithCompletion = '(x) ' + todo.todoText;
+      } else {
+        todoTextWithCompletion = '( ) ' + todo.todoText;
+      }
+      
+      todoLi.id = position;
+      todoLi.textContent = todoTextWithCompletion;
+      todoLi.appendChild(this.createDeleteButton());
+      todosUl.appendChild(todoLi);
+    }, this);
   },
   createDeleteButton: function() {
     var deleteButton = document.createElement('button');
@@ -113,6 +138,3 @@ var view = {
 };
 
 view.setUpEventListeners();
-
-
-
